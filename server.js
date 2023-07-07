d62b7959b28b5b16f3d6a16e5762ef160bd10767
codehashira28 <123044813+codehashira28@users.noreply.github.com>
2023-07-06 22:42:26 -0500

    added code to require all necessary libraries and external files to update db.json file and generate unique id's for notes. Code was also added to set up get and post paths to respond to user input to save notes and generate note list after saving new notes to the db.json file.

--- server.js
+++ server.js
@@ -2,12 +2,29 @@ const express = require('express');
 const path = require('path');
 const PORT = process.env.PORT ?? 3001;
 const app = express();
+const notes = require('./db/db.json');
+const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
+const { log } = require('console');
 
+
+app.use(express.json());
 app.use(express.static('public'));
 
 
 app.get('/notes', (req, res) => {
     return res.sendFile(path.join(__dirname, 'public/notes.html'));
+})
+
+app.get('/api/notes', (req, res) => {
+    res.json(notes);
+})
+
+app.post('/api/notes', (req, res) => {
+    req.body.id = uuidv4();
+    notes.push(req.body);
+    fs.writeFileSync('./db/db.json', JSON.stringify(notes));
+    res.json();
 })
 
 app.get('*', (req, res) => {
